927e1c55a3e7d811416d105616789dd91977e0b6
Lyons Chen <lyons19tw@gmail.com>
2024-06-03 13:01:39 -0700

    update dataloader.py

--- RAG/dataloader.py
+++ RAG/dataloader.py
@@ -2,7 +2,7 @@ from langchain_community.document_loaders import TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
-from langchain_community.llms import LlamaCpp
+from langchain_community.llms import Ollama
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
@@ -25,7 +25,6 @@ documents=text_splitter.split_documents(text_documents)
 SENTENCE_EMBEDDING_MODEL = "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
 ef = HuggingFaceEmbeddings(model_name = SENTENCE_EMBEDDING_MODEL)
 db = Chroma.from_documents(documents, ef)
-db.persist()
 
 ## Retriever: retrive top 5 relevant documents in the embedding space from db
 k = 5
@@ -34,23 +33,9 @@ retrived_documents = retriever.invoke("Thus we may know that there are five esse
 print(f"lenth of retrived_documents: {len(retrived_documents)}")
 print(f"Content of retrived_documents: {retrived_documents}")
 
-## load local LLM: mistral-instruct
+## load local llama3(8B) using Ollama
 
-N_GPU_LAYERS = -1  # -1: trying to use all available gpu layers
-N_CTX = 2000  # context size of the model
-N_BATCH = 512  # Should be between 1 and n_ctx
-
-llm = LlamaCpp(  model_path = 'F:\\unsloth\\unsloth\\meta-llama-3-8b-instruct-imat-Q4_K_M.gguf', # the path of the local llm model
-                 #n_gpu_layers = N_GPU_LAYERS,
-                 n_batch = N_BATCH,
-                 n_ctx = N_CTX,
-                 max_tokens = 200,
-                 verbose = True,
-                 thread = 12
-                 )
-
-#no_context = llm.invoke("What are five essentials for victory")
-#print(f"no_context: {no_context} <end of no_context>\n")
+llm = Ollama(model="llama3:latest")
 
 ## RAG : using local LLM to answer questions according to the retrieved documents
 
@@ -69,8 +54,9 @@ Q: {input} A:""")
 document_chain = create_stuff_documents_chain(llm, prompt)
 retrieval_chain = create_retrieval_chain(retriever, document_chain)
 
-# Run
-QUESTION = "What are the title of the first 3 chapters of the book: the art of war?"
+# Run the query through the defined chain;
+QUESTION = "Who is the author of the book: the art of war?"
 
+# Question -> RETRIEVER ->prepare PROMPT by combining retrived documents and the question -> LLM -> Answer 
 response = retrieval_chain.invoke({"input": QUESTION})
 print(response["answer"])
